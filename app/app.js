653753e9f58f0b0dd835710f120ea4578a65882e
mctwitty <mason.twitty@gmail.com>
2014-12-05 10:49:46 -0800

    nothing much

--- app/app.js
+++ app/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('SuperCoolApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'SuperCoolApp.view1',
+  'SuperCoolApp.view2',
+  'SuperCoolApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
